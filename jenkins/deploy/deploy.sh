2b15a7beb93dfb5745b3955472f3f9be49a6d190
Marcin <mdyksa@gmail.com>
2024-02-22 20:31:25 +0000

    Add

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven > $PWD/.auth
 echo $BUILD_TAG > $PWD/.auth
 echo $PASS > $PWD/.auth
 
-scp $PWD/.auth vagrant@localhost:/tmp/.auth
-scp ./jenkins/deploy/publish vagrant@localhost:/tmp/publish
-ssh vagrant@localhost "/tmp/publish"
+scp -i /opt/id_ed25519 PWD/.auth vagrant@192.168.31.203:/tmp/.auth
+scp -i /opt/id_ed25519 ./jenkins/deploy/publish vagrant@192.168.31.203:/tmp/publish
+ssh vagrant@192.168.31.203 "/tmp/publish"
