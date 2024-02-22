f8c90986eb41caf5081c1921d2d77cbd8793043e
Marcin <mdyksa@gmail.com>
2024-02-22 10:33:46 +0000

    Add

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,6 @@
 echo "***************************"
 echo "******Building jar.....****"
 echo "***************************"
+WORKSPACE=/home/vagrant/docker/jenkins_compose/jenkins_home/workspace/pipeline-project
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-11 $@
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-11 $@
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,4 +5,6 @@ echo "***************************"
 echo "******TESTING.....****"
 echo "***************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-11 $@
+WORKSPACE=/home/vagrant/docker/jenkins_compose/jenkins_home/workspace/pipeline-project
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-11 $@
