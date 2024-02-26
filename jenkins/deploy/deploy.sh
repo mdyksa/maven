a51be13884341f9f248e6798912da808ef27f710
Marcin <mdyksa@gmail.com>
2024-02-26 09:43:50 +0000

    Add post actions in jenkinsfile

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo maven > $PWD/.auth
-echo $BUILD_TAG > $PWD/.auth
-echo $PASS > $PWD/.auth
+echo $BUILD_TAG >> $PWD/.auth
+echo $PASS >> $PWD/.auth
 
 scp -i /opt/id_ed25519 $PWD/.auth vagrant@192.168.31.203:/tmp/.auth
 scp -i /opt/id_ed25519 ./jenkins/deploy/publish vagrant@192.168.31.203:/tmp/publish
