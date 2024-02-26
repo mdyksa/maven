303d7bf59818bc2b07d41bfd3a3465fac3193d37
Marcin <mdyksa@gmail.com>
2024-02-26 09:27:32 +0000

    Add post actions in jenkinsfile

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,5 +3,4 @@
 cp -f java-app/target/*.jar  jenkins/build/
 
 echo "Building docker image"
-cd jenkins/build/ && docker-compose -f docker-compose-build.yaml build --no-cache .
-
+cd jenkins/build/ && docker-compose -f docker-compose-build.yaml build --no-cache
